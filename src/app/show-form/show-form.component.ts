0e6cb703c6350e7bbc576ad3e0c0336ce012ce25
Paul Barrett <paulbarrett@Pauls-MacBook-Air.local>
2018-10-23 14:15:02 -0600

    working post

--- src/app/show-form/show-form.component.ts
+++ src/app/show-form/show-form.component.ts
@@ -19,14 +19,30 @@ export class ShowFormComponent implements OnInit {
   constructor(){}
 
 
-  onSubmit(){ this.submitted = true; fetch('https://hapi-practice-ftjjbqgvls.now.sh/shows').then(function(response) {
-    return response.json()
-  }).then(function(myJson) {
-    console.log(myJson);
-  });
+  onSubmit(){ this.submitted = true; fetch('https://hapi-practice-ftjjbqgvls.now.sh/shows', {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, cors, *same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, same-origin, *omit
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            // "Content-Type": "application/x-www-form-urlencoded",
+        },
+        redirect: "follow", // manual, *follow, error
+        referrer: "no-referrer", // no-referrer, *client
+        body: this.convertShow()  // body data type must match "Content-Type" header
+    })
+    .then(response => response.json());
+  } // parses response to JSON
+  
+  // .then(function(response) {
+  //   return response.json()
+  // }).then(function(myJson) {
+  //   console.log(myJson);
+  // });
   // console.log(this.convertShow)
     // return this.convertShow;
-  };
+  // };
   ngOnInit(){
     
   
@@ -40,10 +56,11 @@ export class ShowFormComponent implements OnInit {
   convertShow(){
     const test = JSON.stringify(this.model);
     console.log(test);
-    console.log(typeof(test))
+    // console.log(typeof(test))
     const newJsonObject=JSON.parse(test)
     console.log(newJsonObject);
-    console.log(typeof(newJsonObject))
+    // console.log(typeof(newJsonObject))
+    return test
   }
   
 
